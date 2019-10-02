7d31ea00d956f2403e196ee0c471bafc5b445676
Waldyr de Souza <waldyr.ar@gmail.com>
2019-10-02 13:18:18 +0200

    Update organization_spec.rb

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -6,21 +6,21 @@ describe Organization do
    @organization =  FactoryGirl.create(:organization)
   end
 
-  it 'creates a new organizaiton'  do 
+  it 'creates a new organization'  do 
     @organization.should be_valid
     @organization.should_not be_nil
   end
 
   it 'should require name'  do 
-    organizaiton = Organization.create(:url => 'www.organization.com')
-    organizaiton.should_not be_valid
-    organizaiton.errors.full_messages.should include "Name can't be blank"	
+    organization = Organization.create(:url => 'www.organization.com')
+    organization.should_not be_valid
+    organization.errors.full_messages.should include "Name can't be blank"	
   end
 
   it 'should require url' do 
-    organizaiton = Organization.create(:name => 'My organization')
-    organizaiton.should_not be_valid
-    organizaiton.errors.full_messages.should include "Url can't be blank"
+    organization = Organization.create(:name => 'My organization')
+    organization.should_not be_valid
+    organization.errors.full_messages.should include "Url can't be blank"
   end
   
   
